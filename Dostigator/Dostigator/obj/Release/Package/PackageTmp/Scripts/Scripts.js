d56905d06dbf76a02f727b974bb2b9505c37641d
Евгений Вайда <e.v.e.r.e.s.tt.1551@gmail.com>
2018-06-11 22:40:46 +0300

    some improvements

---
+++ Dostigator/Dostigator/obj/Release/Package/PackageTmp/Scripts/Scripts.js
@@ -0,0 +1,9 @@
+﻿function ShowImagePreview(Img, previewImage) {
+    if (Img.files && Img.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $(previewImage).attr('src', e.target.result);
+        }
+        reader.readAsDataURL(Img.files[0]);
+    }
+}
